0d84147ab78a99ebd96da841ea41adf921dcc8f3
alexalehno <alekswakeup@gmail.com>
2023-07-15 08:34:10 +0300

    refactor: add RTK query, redux-persist

--- src/store/gameSlice.js
+++ src/store/gameSlice.js
@@ -1,168 +1,57 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-
-export const fetchData = createAsyncThunk(
-  'game/fetchData',
-
-  async function (_, { rejectWithValue }) {
-    try {
-      const response = await fetch('https://raw.githubusercontent.com/alexalehno/image-data/master/data.json');
-
-      if (!response.ok) {
-        throw new Error('Server error!');
-      }
-
-      const data = await response.json();
-
-      return data;
-
-    } catch (error) {
-      return rejectWithValue(error.message);
-    }
-  }
-);
 
 export const gameSlice = createSlice({
   name: 'game',
 
   initialState: {
-    data: [],
-    status: null,
-    error: null,
-    answeredQuestionsInCategory: [],
-    passedCaterogies: [],
-    rightNumInCategory: null,
-    totalNumInCategory: null,
-    type: null,
-    category: null,
-    options: [],
+    type: '',
+    category: 1,
     currentQuestion: 0,
     lastQuestion: null,
-    isRight: null,
-    pathname: '',
-    isPassedCaterogy: false,
+    completedQuestions: [],
+    completedCaterogies: [],
     isAnswered: false,
+    isPassedCaterogy: false,
     isQuit: false,
-
-    settings: {
-      volume: 0,
-      isTimeGame: false,
-      timeToAnswer: 5,
-    },
-
-    defaultSettings: {
-      volume: 0,
-      isTimeGame: false,
-      timeToAnswer: 5,
-    },
-    
-    timeout: 5,
+    pathname: '',
   },
 
   reducers: {
-    setTime(state, action) {
-      state.timeout = action.payload.timeout;
-    },
-
-    saveSettings(state, action) {
-      state.settings = { ...action.payload };
-    },
-
-    getLocalStorage(state, action) {
-      let path = localStorage.getItem('pathname');
-      let cats = localStorage.getItem('categories');
-      let settings = localStorage.getItem('settings');
-
-      if (cats) {
-        state.passedCaterogies = JSON.parse(localStorage.getItem('categories'));
-      }
-
-      if (path) {
-        state.pathname = path;
-      }
-
-      if (settings) {
-        state.settings = JSON.parse(localStorage.getItem('settings'));
-      }
-    },
-
-    setLocalStorage(state, action) {
-      localStorage.setItem('pathname', state.pathname);
-      localStorage.setItem('categories', JSON.stringify(state.passedCaterogies));
-      localStorage.setItem('settings', JSON.stringify(state.settings));
-    },
-
-    setOptions(state, action) {
-      state.options = action.payload.options;
-    },
-
     setCurrentQuestion(state, action) {
-      let { category, type } = action.payload;
-
-      if (category === '0') {
-        category = 1;
-      }
+      const { category, type } = action.payload;
 
       state.type = type;
-      state.category = Number(category);
+      state.category = category;
       state.currentQuestion = type === 'artist' ? (category - 1) * 10 : (category - 1) * 10 + 120;
       state.lastQuestion = state.currentQuestion + 9;
-    },
-
-    resetCategoryInfo(state, action) {
-      state.rightNumInCategory = null;
-      state.totalNumInCategory = null;
-      state.answeredQuestionsInCategory = [];
     },
 
     setPathname(state, action) {
       state.pathname = action.payload.pathname;
     },
 
-    setAnswer(state, action) {
-      state.isRight = action.payload.isRight;
+    resetCompletedQuestions(state) {
+      state.completedQuestions = [];
     },
 
-    addQuestion(state, action) {
-      const { data, currentQuestion, answeredQuestionsInCategory, isRight } = state;
-
-      const answeredQuestion = {
-        ...data[currentQuestion],
-        isRight,
-      }
-
-      state.answeredQuestionsInCategory = [...answeredQuestionsInCategory, answeredQuestion];
-    },
-
-    nextQuestion(state, action) {
+    nextQuestion(state) {
       state.currentQuestion += 1;
     },
 
-    getResultCategory(state, action) {
-      const { answeredQuestionsInCategory } = state;
-
-      state.rightNumInCategory = answeredQuestionsInCategory.filter(el => el.isRight).length;
-      state.totalNumInCategory = answeredQuestionsInCategory.length;
+    addAnswer(state, action) {
+      const { completedQuestions } = state;
+      state.completedQuestions = [...completedQuestions, action.payload.answer];
     },
 
     addCategory(state, action) {
-      const { type, category, rightNumInCategory, totalNumInCategory, answeredQuestionsInCategory, passedCaterogies } = state;
+      const { type, category, completedCaterogies } = state;
 
-      const filteredCaterogies = passedCaterogies.filter(item =>
-        (item.type !== type && item.category !== category)
-        || (item.type !== type && item.category === category)
-        || (item.type === type && item.category !== category)
+      const filteredCaterogies = completedCaterogies.filter(item =>
+        !(item.type === type && item.category === category)
       )
 
-      const passedCaterogy = {
-        type: type,
-        category: category,
-        right: rightNumInCategory,
-        total: totalNumInCategory,
-        questions: [...answeredQuestionsInCategory],
-      }
-
-      state.passedCaterogies = [...filteredCaterogies, passedCaterogy];
+      state.completedCaterogies = [...filteredCaterogies, action.payload.category];
     },
 
     handleQuestionWindow(state, action) {
@@ -177,41 +66,16 @@ export const gameSlice = createSlice({
       state.isQuit = action.payload.value;
     },
   },
-
-  extraReducers: {
-    [fetchData.pending]: (state, action) => {
-      state.status = 'loading';
-      state.error = null;
-    },
-
-    [fetchData.fulfilled]: (state, action) => {
-      state.status = 'resolved';
-      state.data = action.payload;
-    },
-
-    [fetchData.rejected]: (state, action) => {
-      state.status = 'rejected';
-      state.error = action.payload;
-    },
-  }
 });
-
 
 export const {
   setCurrentQuestion,
-  setAnswer,
   nextQuestion,
-  resetCategoryInfo,
+  resetCompletedQuestions,
   setPathname,
-  addQuestion,
-  getResultCategory,
+  addAnswer,
   addCategory,
   handleQuestionWindow,
   handleCategoryWindow,
   handleQuitWindow,
-  setOptions,
-  getLocalStorage,
-  setLocalStorage,
-  setTime,
-  saveSettings
 } = gameSlice.actions;
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,43 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { gameSlice } from './gameSlice';
- 
+import { apiSlice } from './apiSlice';
+import { settingsSlice } from './settingsSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+
+const rootReducer = combineReducers({
+  game: gameSlice.reducer,
+  settings: settingsSlice.reducer,
+  [apiSlice.reducerPath]: apiSlice.reducer
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['game', 'settings']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    game: gameSlice.reducer,
-  },
+  reducer: persistedReducer,
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(apiSlice.middleware),
 });
+
+export const persistor = persistStore(store);
